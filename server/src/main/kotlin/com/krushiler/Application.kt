785fe422177fc913753a50cae0764c2ad4325136
Lazarev Daniil <daniil.kn2004@gmail.com>
2023-10-28 20:32:00 +0800

    [Server]: Move server to 8081 port

--- server/src/main/kotlin/com/krushiler/Application.kt
+++ server/src/main/kotlin/com/krushiler/Application.kt
@@ -11,7 +11,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 

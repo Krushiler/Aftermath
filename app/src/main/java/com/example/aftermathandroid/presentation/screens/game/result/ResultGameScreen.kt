fcdd8081231180127ee72abb4787f719fa65d616
Lazarev Daniil <daniil.kn2004@gmail.com>
2023-12-01 13:11:09 +0800

    [Android]: Add back button to result game screen

--- app/src/main/java/com/example/aftermathandroid/presentation/screens/game/result/ResultGameScreen.kt
+++ app/src/main/java/com/example/aftermathandroid/presentation/screens/game/result/ResultGameScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,16 +19,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.aftermathandroid.R
 import com.example.aftermathandroid.presentation.common.component.Gap
+import com.example.aftermathandroid.presentation.common.provider.LocalRootNavigationOwner
+import com.example.aftermathandroid.presentation.common.provider.storeViewModel
+import com.example.aftermathandroid.presentation.navigation.root.RootNavigationViewModel
 import com.example.aftermathandroid.presentation.theme.Dimens
 
 @Composable
 fun ResultGameScreen(
+    rootNavigationViewModel: RootNavigationViewModel = storeViewModel(LocalRootNavigationOwner),
     viewModel: ResultGameViewModel = hiltViewModel()
 ) {
     val state = viewModel.stateFlow.collectAsState()
 
     Scaffold { padding ->
-        Box(modifier = Modifier.padding(padding)) {
+        Box(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
             Column(
                 modifier =
                 Modifier
@@ -57,6 +67,16 @@ fun ResultGameScreen(
                         text = state.value.score.toString(),
                         style = MaterialTheme.typography.headlineLarge,
                     )
+                }
+            }
+            Box(modifier = Modifier.padding(Dimens.md)) {
+                OutlinedButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter),
+                    onClick = { rootNavigationViewModel.back() },
+                ) {
+                    Text(text = stringResource(id = R.string.back))
                 }
             }
         }
